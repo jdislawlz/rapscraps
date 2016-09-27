181eee7c0ad0ba41c017d85a99f4e7a00f3b756b
Josh Delgado <accounts@joshdelgado.me>
2016-09-27 16:12:52 -0400

    Twitter API Integration
    Set up twitter api to pull 5 most recent tweets and display them

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+$client = Twitter::REST::Client.new do |config|
+	config.consumer_key        = '7QdrcaTdSli5PQoJeG2uZhCUw'
+	config.consumer_secret     = 'RECbZSi7QiJEbFUMLpBNwgkAhUadCJxP0YUyqXaitTDltIYtca'
+	config.access_token = '170548989-eKCg3cTQnDKjSetyEUW4YBIpqbbW0JPqjruGN1os'
+	config.access_token_secret = 'JCjQsEPXba6UkzVd0OrDGNsTTG7CR75PS5yHXHJw69iMW'
+end
