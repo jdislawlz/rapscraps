49c5c85051b2c1de36544187538c5e5bcdc44d7d
Josh Delgado <accounts@joshdelgado.me>
2016-09-28 14:23:44 -0400

    Added styles
    Added bootstrap from CDN, started SASS styling, added Rap Scraps logo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
 	def index
-		@tweets = $client.user_timeline('Rap_scraps', count: 5)
+		@tweets = $client.user_timeline('Rap_scraps', count: 10)
 	end
 end
