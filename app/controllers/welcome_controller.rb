7bbee7a54bef0ef2b8f40857a974ded3fed89f42
jdislawlz <accounts@joshdelgado.me>
2017-03-21 15:11:33 -0400

    Precompile to fix css/js on Heroku
    Fingers crossed.
    
    Also started the logic to combine multi tweet scraps

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,19 @@ class WelcomeController < ApplicationController
 	require 'date'
 	def index
 		@tweets = $client.user_timeline('Rap_scraps', count: 10)
+		@new = []
 		@tweets.each do |tweet|
-			if tweet.full_text.include? '(\(\d+[\/,\\]\d+\))'
-			   puts "String is a multiple"
+			if tweet.full_text[/\(\d+[\/,\\]\d+\)/]
+				first = tweet.full_text[/\(\d+[\/,\\]\d+\)/]
+			  puts tweet.full_text[/\(\d+[\/,\\]\d+\)/]
+				@new << "yess"
+			else
+				puts "cool"
+				@new << tweet.full_text
 			end
+		end
+		@new.each do |newt|
+			puts newt
 		end
 	end
 end
